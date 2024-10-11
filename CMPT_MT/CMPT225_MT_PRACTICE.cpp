a61c0b44ad1e38c936c6a00af6be82f2a5715d86
Alex Chen <alexanderchen817@gmail.com>
2024-10-10 21:50:57 -0700

    added test cases

--- CMPT_MT/CMPT225_MT_PRACTICE.cpp
+++ CMPT_MT/CMPT225_MT_PRACTICE.cpp
@@ -6,17 +6,22 @@ using std::endl;
 using std::getline;
 using std::string;
 
+
 int main()
 {
     LinkedList list;
     string text = "hello";
     list.insertBack(text);
-    list.printList();
+    // list.printList();
     list.insertFront("hi");
-    cout << endl;
-    list.printList();
+    // cout << endl;
+    // list.printList();
     list.insertBack("DO NOT REDEEM");
-    cout << endl;
+    // cout << endl;
+    // list.printList();
+    // cout << list.getItem(1) << endl;
+    list.reverseList();
+    // cout << endl;
     list.printList();
     return 0;
 }
